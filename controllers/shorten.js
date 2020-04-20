23a9b7a3746d4da5bed9a351a3b80fd4af655aae
redorii <arundas0purayil@gmail.com>
2020-04-20 17:02:08 -0400

    Added bootstrap and function to check if URL already exists

--- controllers/shorten.js
+++ controllers/shorten.js
@@ -3,10 +3,40 @@ let URL = mongoose.model('URL')
 
 const { validationResult } = require('express-validator');
 
+async function urlExists(url){
+    let url = await URL.findOne({originalUrl: value})
+        
+    if(url !== null){
+        return null
+    }
+    return url
+    
+}
+
 exports.shorten_url = async function(req, res, next){
     const errors = await validationResult(req);
-    if(!errors.isEmpty()){
-        
+    if(errors.isEmpty()){
+        res.render('index',{errors: errors.errors})
     }
+
+    let urlValue = req.body.url
+    if(await urlExists(urlValue) == null){
+        let url = new URL()
+
+        url.originalUrl = urlValue;
+        url.setShortenUrl(urlValue)
+
+        url.save(err => {
+            if(err){
+                return next(err)
+            }
+            res.redirect('/')
+        })
+    }else{
+
+    }
+
+
+    
     res.send(req.body.url)
 }
--- validator.js
+++ validator.js
@@ -1,5 +1,8 @@
+let mongoose = require('mongoose')
+let URL = mongoose.model('URL')
 const { check } = require('express-validator');
 
 exports.validURL = [
     check('url','Enter a valid URL.').isURL(),
+
 ]
